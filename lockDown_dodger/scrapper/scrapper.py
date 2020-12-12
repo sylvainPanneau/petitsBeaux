9cbfcf51f8d19a028da025068de6f1009dbe09fe
ouicmoi <fkorangina@gmail.com>
2020-12-12 03:55:26 +0100

    petitsBeaux

--- lockDown_dodger/scrapper/scrapper.py
+++ lockDown_dodger/scrapper/scrapper.py
@@ -2,14 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
+options = Options()
+options.set_preference("browser.download.folderList",2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir","./")
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+options.set_preference("pdfjs.disabled", True)
+options.set_preference("plugin.disable_full_page_plugin_for_types", "application/pdf")
+browser = webdriver.Firefox(firefox_options=options)
 
-browser = webdriver.Firefox()
-browser.get("https://www.latlong.net/Show-Latitude-Longitude.html")
-browser.find_element(By.ID, "latitude").send_keys("48.819626" + Keys.ENTER) #récupérer lat/long avec le main
-browser.find_element(By.ID, "longitude").send_keys("2.404811" + Keys.ENTER)
-adress = browser.find_element(By.ID, "address").text
-print(adress)
+browser.get("https://media.interieur.gouv.fr/deplacement-covid-19/")
+browser.find_element(By.ID, "field-firstname").send_keys("48.819626")
+browser.find_element(By.ID, "field-lastname").send_keys("2.404811")
+browser.find_element(By.ID, "field-birthday").send_keys("01/01/1900")
+browser.find_element(By.ID, "field-placeofbirth").send_keys("2.404811")
+browser.find_element(By.ID, "field-address").send_keys("2.404811")
+browser.find_element(By.ID, "field-zipcode").send_keys("75001")
+browser.find_element(By.ID, "field-city").send_keys("Paris")
+browser.find_element(By.ID, "field-heuresortie").send_keys("02:16")
+browser.find_element(By.ID, "checkbox-achats_culturel_cultuel").click()
+browser.find_element(By.ID, "generate-btn").click()
