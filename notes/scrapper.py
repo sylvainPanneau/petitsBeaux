494e9478fad68012641a6a023e09309529ce6f06
ouicmoi <fkorangina@gmail.com>
2020-12-20 18:12:23 +0100

    petitsBeaux

--- notes/scrapper.py
+++ notes/scrapper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from time import time
 
 
 firefox_options =   Options()
@@ -15,11 +16,25 @@ browser.find_element(By.ID, "password").send_keys("0114016375T")
 browser.find_element(By.ID, "submit").click()
 moyenne = browser.find_element_by_class_name('ligneResumeMoyenne').text
 
-def alert_new_note(moyenne):
-    return float(moyenne) != 16.91 
+def note_loop_check():
+    test = True
+    moyenne = browser.find_element_by_class_name('ligneResumeMoyenne').text
+    keep_checking = "y"
+    i=0
+    start = time()
+    while test and keep_checking=="y":
+        test = float(moyenne) == 16.91
+        i+=1
+        anchor = time()
+        if anchor-start > 1200:
+            keep_checking = input("Keep scanning for new notes ? (y/n)")
+            start = time()
+            print(moyenne)
+    return test
 
-x=alert_new_note(moyenne)
-if x:
+print(moyenne)
+x=note_loop_check()
+if not x:
     print("Nouvelle note en ligne !")
 else:
     print("RAS")
