50d8609fa584717fe3520526b686e46569696808
ouicmoi <fkorangina@gmail.com>
2020-12-20 21:48:50 +0100

    petitsBeaux

--- notes/scrapper.py
+++ notes/scrapper.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from time import time
+from time import sleep
 
 
+print("\n Scanning... \n")
 firefox_options =   Options()
 firefox_options.add_argument("--headless")
 firefox_options.page_load_strategy = 'none'
@@ -18,24 +20,27 @@ moyenne = browser.find_element_by_class_name('ligneResumeMoyenne').text
 
 def note_loop_check():
     test = True
-    moyenne = browser.find_element_by_class_name('ligneResumeMoyenne').text
     keep_checking = "y"
-    i=0
     start = time()
+    print("\n Scanning... You will be notified if something comes up ! \n")
     while test and keep_checking=="y":
+        moyenne = browser.find_element_by_class_name('ligneResumeMoyenne').text
         test = float(moyenne) == 16.91
-        i+=1
         anchor = time()
         if anchor-start > 1200:
             keep_checking = input("Keep scanning for new notes ? (y/n)")
             start = time()
-            print(moyenne)
+            print("Moyenne :", moyenne)
+            print("\n Scanning... \n")
     return test
 
-print(moyenne)
-x=note_loop_check()
+print("Moyenne :", moyenne)
+keep_checking = input("Keep scanning for new notes ? (y/n) : ")
+if keep_checking == "y":
+    x=note_loop_check()
+else:
+    quit()
 if not x:
     print("Nouvelle note en ligne !")
 else:
     print("RAS")
-
